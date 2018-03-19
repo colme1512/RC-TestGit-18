f61849c2f673efe133d4b58d36b79a0c6e287468
rebecacortazar <rebeca.cortazar@deusto.es>
2018-03-19 18:31:06 +0100

    Condiciones iniciales Laboratorio 17-18

--- src/game/Barbarian.java
+++ src/game/Barbarian.java
@@ -11,7 +11,6 @@ public class Barbarian extends Character {
 	// TODO Auto-generated method stub
 
 		System.out.println("You are a strong, hulky barbarian.");
-		System.out.println("Testing YouTrack Done change.");
 		
 	}
 }
